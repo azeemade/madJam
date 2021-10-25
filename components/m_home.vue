947d849bd65ccf66382f093ba189220be2518294
Azeem Adenuga <adenugaazeem80@gmail.com>
2021-10-25 08:31:21 +0100

    update to mobile homepage

--- components/m_home.vue
+++ components/m_home.vue
@@ -5,6 +5,38 @@
         <div class="px-3 mt-16">
             <div class="">
                 <div class="d-flex justify-content-between mb-4">
+                    <p class="text-xl text--dark font-medium">For you</p>
+                </div>
+                
+                <playlistcard />
+            </div>
+
+            <div class="mt-9">
+                <div class="d-flex justify-content-between mb-4">
+                    <p class="text-xl text--dark font-medium">Popular playlists</p>
+                    <nuxt-link to="/categories" class="text--dark text-decoration-underline">
+                        view all
+                    </nuxt-link>
+                </div>
+                
+                <playlistcard />
+            </div>
+
+            <div class="mt-9">
+                <div class="d-flex justify-content-between mb-4">
+                    <p class="text-xl text--dark font-medium">Gift playlists</p>
+                </div>
+                <div class="py-5 bg--light px-3">
+                    <p class="text-2xl">Get a special playlist for you or that special someone today</p>
+                    <nuxt-link to="/gift" class="btn bg--dark text--light float-end">
+                        Get playlist
+                        <i class="bi bi-arrow-right"></i>
+                    </nuxt-link>
+                </div>
+            </div>
+
+            <div class="mt-9">
+                <div class="d-flex justify-content-between mb-4">
                     <p class="text-xl text--dark font-medium">Category</p>
                     <nuxt-link to="/categories" class="text--dark text-decoration-underline">
                         view all
@@ -12,20 +44,6 @@
                 </div>
             
                 <categorycard />
-            </div>
-
-            <div class="mt-9">
-                <div class="d-flex justify-content-between mb-4">
-                    <p class="text-xl text--dark font-medium">For you</p>
-                </div>
-                
-                <playlistcard />
-            </div>
-
-            <div class="d-flex justify-center my-5">
-                <nuxt-link to="/playlists" class="btn bg--dark text--primary btn-lg rounded-pill">
-                    View all playlists
-                </nuxt-link>
             </div>
 
             <subscribemodal />
