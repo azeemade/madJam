0aa3e57ffa1ad543555e8d239f62180fa74e56d5
Azeem Adenuga <adenugaazeem80@gmail.com>
2021-11-06 16:07:37 +0100

    Base commit

---
+++ src/components/mobile/PlaylistCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="overflow-cards">
+        <div class="playlist-card-sm mr-4" v-for="item, index in playlists" :key="index">
+            <img :src="item.playlist_image" alt="" class="rounded-lg mb-3 playlist_image" width="136" height="112">
+            <p class="text--dark mb-1 font-semibold">{{item.playlist_title}}</p>
+            <p class=" text-xs text--dark-50">{{item.songs_count}} songs</p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'PlaylistCard',
+    props:{
+        playlists: {
+            type: Array,
+        }
+    },
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
+<style scoped>
+    .playlist_image{
+        max-width: 136px;
+        max-height: 112px;
+    }
+</style>
