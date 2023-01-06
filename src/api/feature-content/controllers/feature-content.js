2b900ed6f63b1901e6eb1bacf38092086dcf2159
Azeem Adenuga <adenugaazeem80@gmail.com>
2023-01-06 10:29:52 +0100

    initial commit

---
+++ src/api/feature-content/controllers/feature-content.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * feature-content controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::feature-content.feature-content');
---
+++ src/api/feature-content/services/feature-content.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * feature-content service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::feature-content.feature-content');
